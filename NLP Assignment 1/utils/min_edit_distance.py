a4c60a29a2e50844814d616d75fcdf0fa9ba95c6
kruthika01 <89992890+kruthika01@users.noreply.github.com>
2024-01-29 16:49:37 -0500

    Update min_edit_distance.py

--- NLP Assignment 1/utils/min_edit_distance.py
+++ NLP Assignment 1/utils/min_edit_distance.py
@@ -3,30 +3,30 @@ def med_function(word1, word2, show_matrix):
     rows = len(word1)+1 
     cols = len(word2)+1
 
-    distance = [
+    T = [
         [0 for x in range(cols)] 
         for x in range(rows)]
 
     for i in range(1, rows):
-        distance[i][0] = i
+        T[i][0] = i
 
     for j in range(1, cols):
-        distance[0][j] = j
+        T[0][j] = j
         
     for col in range(1, cols):
         for row in range(1, rows):
-            if word1[row-1] == word2[col-1]:
+            if str1[row-1] == str2[col-1]:
                 sub_cost = 0
             else:
                 sub_cost = 1
-            distance[row][col] = min(distance[row-1][col] + 1,      
-                                     distance[row][col-1] + 1,    
-                                     distance[row-1][col-1] + sub_cost)
+            T[row][col] = min(T[row-1][col] + 1,      
+                                     T[row][col-1] + 1,    
+                                     T[row-1][col-1] + sub_cost)
     
     if show_matrix:
-        print("The MED matrix between " + word1 + " and " + word2 + " is: ")
+        print("The MED matrix between " + str1 + " and " + str2 + " is: ")
         for r in range(rows):
-            print(distance[r])
+            print(T[r])
     
-    return distance[row][col]
+    return T[row][col]
 
